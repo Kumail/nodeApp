db5105206c402b0dd83e536ae72fe75a8206e98a
Kumail <k142227@nu.edu.pk>
2023-01-02 19:55:17 +0500

    Added multiple REST APIs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -13,6 +14,29 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const note = notes.find(note => {
+        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+        return note.id === id
+    })
+    console.log(note)
+
+    if(note){
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
