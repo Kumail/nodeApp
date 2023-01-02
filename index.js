0614539f1c5f0596ed7df6e2e051676cd4189c78
Kumail <k142227@nu.edu.pk>
2023-01-02 22:24:56 +0500

    Added mock requests

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     { id: 1, content: "HTML is easy", date: "2022-05-30T17:30:31.098Z", important: true },
     { id: 2, content: "Browser can execute only Javascript", date: "2022-05-30T18:39:34.091Z", important: false },
@@ -38,6 +40,33 @@ app.delete('/api/notes/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxId
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
